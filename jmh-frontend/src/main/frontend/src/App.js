531abaedbe79565717b66c1acfa5b81ab9c9b458
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2023-09-08 02:13:56 +0300

    Dynamic URL to backend

--- jmh-frontend/src/main/frontend/src/App.js
+++ jmh-frontend/src/main/frontend/src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Define the API URL here using the environment variable
+    const apiUrl = process.env.REACT_APP_API_URL;
+
+    // Fetch data from the "/api/benchmark" endpoint
+    fetch(`${apiUrl}/api/benchmark`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {isLoading
+            ? "Loading..."
+            : // Display the fetched data
+              data.map((benchmark) => (
+                <div key={benchmark.id}>
+                  <h2>{benchmark.name}</h2>
+                  {/* Display other properties of the benchmark */}
+                </div>
+              ))}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
