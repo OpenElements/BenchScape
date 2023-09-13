b1afbc9153faeefd2e828e93333c7281a3f949b2
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-09-13 10:08:51 +0200

    doc

--- jmh-common/src/main/java/module-info.java
+++ jmh-common/src/main/java/module-info.java
@@ -1,3 +1,6 @@
+/**
+ * Module that defines the data model classes for JMH benchmark executions.
+ */
 module com.openelements.benchscape.common {
     exports com.openelements.benchscape.common;
 
--- jmh-common/src/test/java/module-info.java
+++ jmh-common/src/test/java/module-info.java
@@ -1,3 +1,6 @@
+/**
+ * Test module for the common module {@link com.openelements.benchscape.common/}.
+ */
 open module com.openelements.benchscape.common.test {
     requires com.openelements.benchscape.common;
     requires org.junit.jupiter.api;
