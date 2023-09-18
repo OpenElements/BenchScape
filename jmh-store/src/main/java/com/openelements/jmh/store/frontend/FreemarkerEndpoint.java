e78d92f49c66bd70839f9c9e123d895d9e4c3585
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2023-09-19 00:05:34 +0300

    fix CORS

--- jmh-store/src/main/java/com/openelements/jmh/store/frontend/FreemarkerEndpoint.java
+++ jmh-store/src/main/java/com/openelements/jmh/store/frontend/FreemarkerEndpoint.java
@@ -19,11 +19,13 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@CrossOrigin
 public class FreemarkerEndpoint {
 
     private final DataService dataService;
