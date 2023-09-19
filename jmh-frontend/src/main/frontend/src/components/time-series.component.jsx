6f1634920e4f04726a90aa818203f7f9d9878de0
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-09-19 12:13:46 +0200

    show data in frontend

--- jmh-frontend/src/main/frontend/src/components/time-series.component.jsx
+++ jmh-frontend/src/main/frontend/src/components/time-series.component.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { useTimeSeries } from "../hooks/hooks";
-import { useParams } from "react-router-dom";
+import {useTimeSeries} from "../hooks/hooks";
+import {useParams} from "react-router-dom";
 
-const TimeSeriesComponent = ({ type }) => {
-  const { id } = useParams();
-  const { data } = useTimeSeries(id, type);
+const TimeSeriesComponent = ({type}) => {
+  const {id} = useParams();
+  const {data} = useTimeSeries(id);
 
   console.log(data, "getting data");
 
   return (
-    <React.Fragment>
-      {/* TODO: Return a loading state once the data is still loading */}
-      {data && <div dangerouslySetInnerHTML={{ __html: data }}></div>}
-    </React.Fragment>
+      <React.Fragment>
+        {/* TODO: Return a loading state once the data is still loading */}
+        {data && <div dangerouslySetInnerHTML={{__html: data}}></div>}
+      </React.Fragment>
   );
 };
 
--- jmh-frontend/src/main/frontend/src/hooks/hooks.js
+++ jmh-frontend/src/main/frontend/src/hooks/hooks.js
@@ -1,5 +1,5 @@
 import useSwr from "swr";
-import { dataFetcher } from "../api/api";
+import {dataFetcher} from "../api/api";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -7,8 +7,8 @@ export function useBenchMarks() {
   return useSwr(`${apiUrl}/api/benchmark`, dataFetcher);
 }
 
-export function useTimeSeries(id, type) {
-  return useSwr(`${apiUrl}/timeseries/${type}?id=${id}`, dataFetcher);
+export function useTimeSeries(id) {
+  return useSwr(`${apiUrl}/api/timeseries/${id}`, dataFetcher);
 }
 
 // SWR documentation: https://swr.vercel.app/docs/getting-started
