8285f503365f55e0dcc5e61fefead78f639aa1cc
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2023-09-25 23:20:02 +0300

    change ge to de

--- jmh-frontend/src/main/frontend/src/i18n.js
+++ jmh-frontend/src/main/frontend/src/i18n.js
@@ -1,12 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enJSON from "./translations/en/translation.json";
-import geJSON from "./translations/ge/translation.json";
+import geJSON from "./translations/de/translation.json";
 
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-export const availableLanguages = ["en", "ge"];
+export const availableLanguages = ["en", "de"];
 // don't want to use this?
 // have a look at the Quick start guide
 // for passing in lng and translations on init
@@ -29,7 +29,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    resources: { en: enJSON, ge: geJSON },
+    resources: { en: enJSON, de: geJSON },
     fallbackLng: "en",
     debug: true,
 
