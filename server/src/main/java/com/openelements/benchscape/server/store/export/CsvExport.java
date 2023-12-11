513fa19f2f9379da1776609c6e97c0531960950c
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-12-11 09:59:40 +0100

    More CSV endpoints

--- server/src/main/java/com/openelements/benchscape/server/store/export/CsvExport.java
+++ server/src/main/java/com/openelements/benchscape/server/store/export/CsvExport.java
@@ -1,6 +1,8 @@
 package com.openelements.benchscape.server.store.export;
 
+import com.openelements.benchscape.server.store.data.Benchmark;
 import com.openelements.benchscape.server.store.data.Environment;
+import com.openelements.benchscape.server.store.data.Measurement;
 import com.openelements.server.base.csv.CsvColumn;
 import com.openelements.server.base.csv.CsvData;
 import com.openelements.server.base.csv.CsvExportUtils;
@@ -15,6 +17,16 @@ public class CsvExport {
         CsvExportUtils.exportCsv(appendable, csvData, environments);
     }
 
+    public static void exportBenchmarks(final Appendable appendable, List<Benchmark> benchmarks) {
+        final CsvData<Benchmark> csvData = getBenchmarkCsvData();
+        CsvExportUtils.exportCsv(appendable, csvData, benchmarks);
+    }
+
+    public static void exportMeasurements(final Appendable appendable, List<Measurement> benchmarks) {
+        final CsvData<Measurement> csvData = getMeasurementCsvData();
+        CsvExportUtils.exportCsv(appendable, csvData, benchmarks);
+    }
+
     private static CsvData<Environment> getEnvironmentCsvData() {
         List<CsvColumn<Environment>> columns = new ArrayList<>();
         columns.add(new CsvColumn<>("Name", (d, h) -> d.name()));
@@ -22,6 +34,26 @@ public class CsvExport {
         columns.add(new CsvColumn<>("OS", (d, h) -> d.osName() + " " + d.osVersion()));
         columns.add(new CsvColumn<>("Java", (d, h) -> d.jvmName() + " " + d.jvmVersion()));
         CsvData<Environment> csvData = new CsvData<>(columns);
+        return csvData;
+    }
+
+    private static CsvData<Benchmark> getBenchmarkCsvData() {
+        List<CsvColumn<Benchmark>> columns = new ArrayList<>();
+        columns.add(new CsvColumn<>("Name", (d, h) -> d.name()));
+        columns.add(new CsvColumn<>("Parameters", (d, h) -> d.params().toString()));
+        CsvData<Benchmark> csvData = new CsvData<>(columns);
+        return csvData;
+    }
+
+    private static CsvData<Measurement> getMeasurementCsvData() {
+        List<CsvColumn<Measurement>> columns = new ArrayList<>();
+        columns.add(new CsvColumn<>("Timestamp", (d, h) -> d.timestamp().toString()));
+        columns.add(new CsvColumn<>("Value", (d, h) -> d.value() + ""));
+        columns.add(new CsvColumn<>("Min", (d, h) -> d.min() + ""));
+        columns.add(new CsvColumn<>("Max", (d, h) -> d.max() + ""));
+        columns.add(new CsvColumn<>("Error", (d, h) -> d.error() + ""));
+        columns.add(new CsvColumn<>("Unit", (d, h) -> d.unit() + ""));
+        CsvData<Measurement> csvData = new CsvData<>(columns);
         return csvData;
     }
 
--- server/src/main/java/com/openelements/benchscape/server/store/export/ExportEndpoint.java
+++ server/src/main/java/com/openelements/benchscape/server/store/export/ExportEndpoint.java
@@ -2,16 +2,30 @@ package com.openelements.benchscape.server.store.export;
 
 import static com.openelements.benchscape.server.store.endpoints.EndpointsConstants.V2;
 
+import com.openelements.benchscape.jmh.model.BenchmarkUnit;
+import com.openelements.benchscape.server.store.data.Benchmark;
 import com.openelements.benchscape.server.store.data.Environment;
+import com.openelements.benchscape.server.store.data.Measurement;
+import com.openelements.benchscape.server.store.data.MeasurementQuery;
+import com.openelements.benchscape.server.store.services.BenchmarkService;
 import com.openelements.benchscape.server.store.services.EnvironmentService;
+import com.openelements.benchscape.server.store.services.MeasurementService;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStreamWriter;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,20 +36,58 @@ public class ExportEndpoint {
 
     private final EnvironmentService environmentService;
 
+    private final BenchmarkService benchmarkService;
+
+    private final MeasurementService measurementService;
+
     @Autowired
-    public ExportEndpoint(final EnvironmentService environmentService) {
+    public ExportEndpoint(final EnvironmentService environmentService, BenchmarkService benchmarkService,
+            MeasurementService measurementService) {
         this.environmentService = Objects.requireNonNull(environmentService, "environmentService must not be null");
+        this.benchmarkService = Objects.requireNonNull(benchmarkService, "benchmarkService must not be null");
+        this.measurementService = Objects.requireNonNull(measurementService, "measurementService must not be null");
     }
 
     @GetMapping(value = "/environments/csv", produces = "text/csv")
     public @ResponseBody byte[] getCsvExportOfEnvironments() {
-        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-            final List<Environment> all = environmentService.getAll();
-            OutputStreamWriter osw = new OutputStreamWriter(baos);
-            CsvExport.exportEnvironments(osw, all);
+        final List<Environment> all = environmentService.getAll();
+        return createData(osw -> CsvExport.exportEnvironments(osw, all));
+    }
+
+    @GetMapping(value = "/benchmarks/csv", produces = "text/csv")
+    public @ResponseBody byte[] getCsvExportOfBenchmarks() {
+        final List<Benchmark> all = benchmarkService.getAll();
+        return createData(osw -> CsvExport.exportBenchmarks(osw, all));
+    }
+
+    @GetMapping(value = "/measurements/csv", produces = "text/csv")
+    public @ResponseBody byte[] getCsvExportOfMeasurements(@RequestParam final String benchmarkId,
+            @RequestParam(required = false) final BenchmarkUnit unit,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final ZonedDateTime start,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final ZonedDateTime end,
+            @RequestParam(required = false) final List<String> environmentIds) {
+        Objects.requireNonNull(benchmarkId, "benchmarkId must not be null");
+        final BenchmarkUnit queryUnit = Optional.ofNullable(unit).orElse(BenchmarkUnit.OPERATIONS_PER_MILLISECOND);
+        final ZonedDateTime queryStart = Optional.ofNullable(start)
+                .orElse(LocalDateTime.MIN.atZone(ZoneId.systemDefault()));
+        final ZonedDateTime queryEnd = Optional.ofNullable(end)
+                .orElse(LocalDateTime.MAX.atZone(ZoneId.systemDefault()));
+        final List<String> queryEnvironmentIds = Optional.ofNullable(environmentIds)
+                .map(list -> Collections.unmodifiableList(list)).orElse(List.of());
+        final MeasurementQuery query = new MeasurementQuery(benchmarkId, queryUnit,
+                queryStart.toInstant(), queryEnd.toInstant(), queryEnvironmentIds);
+        final List<Measurement> measurements = measurementService.find(query);
+        return createData(osw -> CsvExport.exportMeasurements(osw, measurements));
+    }
+
+    private byte[] createData(final Consumer<Appendable> consumer) {
+        Objects.requireNonNull(consumer, "consumer must not be null");
+        try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            final OutputStreamWriter osw = new OutputStreamWriter(baos);
+            consumer.accept(osw);
             return baos.toByteArray();
         } catch (Exception e) {
-            throw new RuntimeException("Error exporting environments", e);
+            throw new RuntimeException("Error writing bytes", e);
         }
     }
 }
