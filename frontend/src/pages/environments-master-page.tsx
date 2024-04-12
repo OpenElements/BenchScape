32b0abbe487f69cca721abc12b0bec39972596f7
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2024-04-12 05:03:30 +0300

    Use systemMemory instead of systemMemoryReadable to push memory data to the server

--- frontend/src/pages/environments-master-page.tsx
+++ frontend/src/pages/environments-master-page.tsx
@@ -270,11 +270,12 @@ function EnvironmentsPage() {
                                 icon={faMicrochip}
                                 className="ml-2 & mr-2"
                               />
-                              {`${environment.systemProcessors}`}
+                              {`${environment.systemProcessors}`} 
                               <FontAwesomeIcon
                                 icon={faMemory}
                                 className="ml-2 & mr-2"
                               />
+                              {`${environment.systemMemoryReadable}`}
                             </>
                           ) : (
                             <>
