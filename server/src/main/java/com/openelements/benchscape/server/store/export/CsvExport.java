00cd8bc0bb217d46761239065c7a021c2730dd91
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2024-05-08 05:19:05 +0300

    refactor

--- server/src/main/java/com/openelements/benchscape/server/store/export/CsvExport.java
+++ server/src/main/java/com/openelements/benchscape/server/store/export/CsvExport.java
@@ -31,7 +31,8 @@ public class CsvExport {
         final List<CsvColumn<Environment>> columns = new ArrayList<>();
         columns.add(new CsvColumn<>("Name", (d, h) -> d.name() + " "));
         columns.add(new CsvColumn<>("OPERATING SYSTEM", (d, h) -> String.valueOf(d.osFamily()) + " " + d.osVersion() + " "));
-        columns.add(new CsvColumn<>("HARDWARE", (d, h) -> d.systemArch() + " " + String.valueOf(d.systemProcessors()) + " " + d.systemMemory() + " "));
+        columns.add(new CsvColumn<>("HARDWARE", (d, h) -> d.systemArch() + " " + String.valueOf(d.systemProcessors()) + " " +
+                d.systemMemory() + " "));
         columns.add(new CsvColumn<>("Java", (d, h) -> d.jvmName() + " " + d.jvmVersion()+ " "));
         columns.add(new CsvColumn<>("JMH Version", (d, h) -> d.jmhVersion() + " "));
         //columns.add(new CsvColumn<>("Description", (d, h) -> d.description() + " "));
